120e31287ca271ba7b25213a14dfed86c63d0c6d
pisun2 <pisun2@04bc2280-195f-c101-2899-a8bbd5de0aab>
2010-04-25 14:28:43 +0000

    Nguyễn Thế Phú 10 - Other - Source code phần ChonVaHienThiBaiHoc (demo chon va hien thi, nhung hien thi con cham)

---
+++ 10. Other(add-on)/ChonVaHienThiBaiHoc/ChonVaHienThiBaiHoc/Window1.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+
+namespace ChonVaHienThiBaiHoc
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        ReadFile rf;
+        string[] filename;
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {          
+            string path = "\\" + filename[0]; //lay duong dan cua file bai hoc
+            rf.LoadRTFPackage(path, richTextBox1);
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void listBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string sbaichon = listBox1.SelectedItem.ToString();
+
+
+            for (int i = 0; i < filename.Length;i++ )
+            {
+                if (sbaichon == filename[i])
+                {
+                    string path = "\\" + filename[i]; //lay duong dan cua file bai hoc
+                    rf.LoadRTFPackage(path, richTextBox1);
+                }
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            rf = new ReadFile();
+            filename = rf.LoadFiles();
+            for (int i = 0; i < filename.Length; i++)
+            {
+                listBox1.Items.Add(filename[0]);
+            } 
+        }
+    }
+}
