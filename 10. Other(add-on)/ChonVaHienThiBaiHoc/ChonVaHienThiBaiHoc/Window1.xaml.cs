d457df9b528fe59a2a39df0e4309f4b590759dfe
pisun2 <pisun2@04bc2280-195f-c101-2899-a8bbd5de0aab>
2010-04-25 13:41:10 +0000

    Nguyễn Thế Phú 10 - Other - Source code phần ChonVaHienThiBaiHoc (chua hoan chinh con loi)

---
+++ 10. Other(add-on)/ChonVaHienThiBaiHoc/ChonVaHienThiBaiHoc/Window1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+
+namespace ChonVaHienThiBaiHoc
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        ReadFile rf;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            rf = new ReadFile();
+            string[] filename = rf.LoadFiles();
+            //richTextBox1.Name = filename[0]; //thay bang su lua chon bai hoc
+
+            MessageBox.Show(filename[0] - "Lessons/");
+            rf.LoadRTFPackage(filename[0], richTextBox1);
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
