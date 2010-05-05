5f596ac3edd2b0094d51f4cf545f8f9c3f7af60a
lkminh.uns <lkminh.uns@04bc2280-195f-c101-2899-a8bbd5de0aab>
2010-05-05 02:33:44 +0000

    Lương Kiến Minh - 10 - FormDangNhap - Form đăng nhập cho người dùng ( sẽ hoàn thành và tích họp vào project chung sau )

---
+++ trunk/10. Other(add-on)/FormDangNhap/FormDangNhap/Window1.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FormDangNhap
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void btDangNhap_Click(object sender, RoutedEventArgs e)
+        {
+            string ten = this.tbTen.Text;
+            string matKhau = this.tbMatKhau.Text;
+            bool dangNhapThanhCong = DangNhap(ten, matKhau);            
+            if (dangNhapThanhCong == true)
+            {
+                //Viet code de xu ly khi dang nhap thanh cong 
+                //Vi du
+                MessageBox.Show("Dang nhap thanh cong");
+            }
+            else
+            {
+                //Viet code de xu ly khi dang nhap that bai 
+                //Vi du
+                MessageBox.Show("Dang nhap that bai");
+            }
+        }
+
+        public bool DangNhap(string tenDangNhap, string matKhauDangNhap)
+        {
+            //Minh them code xu ly sau
+            //O day, return true hoac false, cac ban co the tu gia dinh de test phan dang nhap 
+            return false;
+        }
+    }
+}
