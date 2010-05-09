d8c8b3a52ebc4da12f49ab02a585c633934f521e
pisun2 <pisun2@04bc2280-195f-c101-2899-a8bbd5de0aab>
2010-05-09 12:38:49 +0000

    Nguyễn Thế Phú - 6.Source Code - Unit test phần load bài học chưa hoàn chỉnh - lần 1

---
+++ 6. Source Code/UnitTest_LoadBaiHoc/ClassToTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using NUnit.Framework;
+using ChonVaHienThiBaiHoc;
+
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class ClassToTest
+    {
+        //Test ham LoadFile
+        [Test]
+        public void UNitTest_LoadFile()
+        {
+            ReadFile rf = new ReadFile();
+            string[] filename = rf.LoadFiles();
+            Assert.AreEqual("Lessons\\chuong1.rtf", filename[0]);
+        }
+
+        //Test ham LoadRTFPackage
+        [Test]
+        public void UNitTest_LoadRTFPackage()
+        {
+            ReadFile rf = new ReadFile();
+            string[] filename = rf.LoadFiles();
+            string path = "\\" + filename[0];
+
+           // ChonVaHienThiBaiHoc.Window1 ch = new Window1();
+           
+           // Assert.AreEqual(1,  rf.LoadRTFPackage(path, ch.return_Rich()));
+        }
+    }
+}
