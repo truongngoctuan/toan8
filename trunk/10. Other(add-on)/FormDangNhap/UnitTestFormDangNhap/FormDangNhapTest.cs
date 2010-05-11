3bdb29335be55adc59fa832af09a252cc94efc6d
lkminh.uns <lkminh.uns@04bc2280-195f-c101-2899-a8bbd5de0aab>
2010-05-11 02:36:26 +0000

    Lương Kiên Minh - 10 - FormDangNhap - Hoàn chỉnh chức năng đăng nhập

--- trunk/10. Other(add-on)/FormDangNhap/UnitTestFormDangNhap/FormDangNhapTest.cs
+++ trunk/10. Other(add-on)/FormDangNhap/UnitTestFormDangNhap/FormDangNhapTest.cs
@@ -4,31 +4,44 @@ using System.Linq;
 using System.Text;
 using NUnit.Framework;
 using DAO;
+using DTO;
+using BUS;
 namespace UnitTestFormDangNhap
 {
     [TestFixture]
     public class FormDangNhapTest
-    {
+    {        
+
         [Test]
-        public void Test_LayDanhSachPupilUser()
+        public void Test_LayDanhSachUser()
         {
             List<string> list = new List<string>();
             list.Add(".svn");
             list.Add("PupilUserName_1");
             list.Add("PupilUserName_2");
-            Assert.AreEqual(list, DAO.PupilUserDAO.LayDanhSachPupilUser());
+            Assert.AreEqual(list, DAO.UserDAO.LayDanhSachTaiKhoan(DTO.UserDTO.DuongDanThuMucPupilAccount));
         }
 
         [Test]
-        public void Test_KiemTraTenDangNhapCoTonTaiKhong()
+        public void Test_KiemTraTaiKhoanCoTonTaiHayKhong()
         {
-            Assert.AreEqual(true, DAO.PupilUserDAO.KiemTraTenDangNhapCoTonTaiKhong("PupilUserName_1"));
+            Assert.AreEqual(true, DAO.UserDAO.KiemTraTaiKhoanCoTonTaiHayKhong("TeacherUserName_1", DTO.UserDTO.DuongDanThuMucTeacherAccount));
         }
 
         [Test]
         public void Test_LayMatKhauCuaTaiKhoan()
         {
-            Assert.AreEqual("pass_1", DAO.PupilUserDAO.LayMatKhauCuaTaiKhoan("PupilUserName_1"));
+            Assert.AreEqual("pass_1", DAO.UserDAO.LayMatKhauCuaTaiKhoan("PupilUserName_1", DTO.UserDTO.DuongDanThuMucPupilAccount));
+        }
+        [Test]
+        public void Test_BUS_KiemTraTaiKhoanCoTonTaiKhong()
+        {
+            Assert.AreEqual(1, BUS.UserBUS.KiemTraTenDangNhapCoTonTaiKhong("TeacherUserName_1"));
+        }
+        [Test]
+        public void Test_BUS_LayMatKhauCuaTaiKhoan()
+        {
+            Assert.AreEqual("pass_2", BUS.UserBUS.LayMatKhauCuaTaiKhoan("TeacherUserName_2"));
         }
     }
 }
