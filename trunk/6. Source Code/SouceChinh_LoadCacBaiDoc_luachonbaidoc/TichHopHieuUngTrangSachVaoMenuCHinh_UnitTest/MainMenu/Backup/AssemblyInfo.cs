bcd8baeebf4d11717998bd12887b381c4c2e4222
kitminh25251325 <kitminh25251325@04bc2280-195f-c101-2899-a8bbd5de0aab>
2010-05-25 05:29:12 +0000

    Vòng Sĩ Kịt - Source Code - Dọn dẹp thùng chứa

--- trunk/6. Source Code/SouceChinh_LoadCacBaiDoc_luachonbaidoc/TichHopHieuUngTrangSachVaoMenuCHinh_UnitTest/MainMenu/Backup/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Resources;
-using System.Reflection;
-
-[assembly: AssemblyTitleAttribute("Color Swatch")]
-[assembly: NeutralResourcesLanguageAttribute("en-US", UltimateResourceFallbackLocation.MainAssembly)]
