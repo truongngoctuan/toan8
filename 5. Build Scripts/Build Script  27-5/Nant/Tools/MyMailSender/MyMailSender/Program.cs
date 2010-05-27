6a89667b3d7548e2c1fb884666953cabcbc51b39
ittrandainghia <ittrandainghia@gmail.com>
2010-05-27 03:58:46 +0000

    build script cho releave mới nhất

---
+++ 5. Build Scripts/Build Script  27-5/Nant/Tools/MyMailSender/MyMailSender/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyMailSender;
+
+namespace MyMailSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] cmdline = Environment.GetCommandLineArgs(); // Get the command line parameter
+            if (cmdline.Length == 7)
+            {
+                MyMailSender.MailSender a = new MailSender();
+                a.SendEmail(cmdline[1], cmdline[2], cmdline[3], cmdline[4], cmdline[5], System.Web.Mail.MailFormat.Text, cmdline[6]);
+            }
+        }
+    }
+}
