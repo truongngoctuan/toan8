fbcdedd1d74651befff35fe5989b3a4f6c1c6894
lkminh.uns <lkminh.uns@04bc2280-195f-c101-2899-a8bbd5de0aab>
2010-05-28 19:27:14 +0000

    Lương Kiến Minh - 6 - Source_BanFinal_2 - Sửa lỗi, hoàn chỉnh chức năng đăng ký và đăng nhập

---
+++ 6. Source Code/Source_BanFinal2/MainMenu/UCTrangBaiTap.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Xml.Linq;
+namespace ColorSwatch
+{
+    /// <summary>
+    /// Interaction logic for UCTrangBaiTap.xaml
+    /// </summary>
+    public partial class UCTrangBaiTap : UserControl
+    {
+        
+        public UCTrangBaiTap()
+        {
+            InitializeComponent();
+        }
+        public UCTrangBaiTap(int iChuong,int iBai)
+        {
+            InitializeComponent();
+            var xElement = XElement.Load(@"BaiTapDaiSo.xml");
+            var baiHocThu = from c in xElement.Descendants("BaiHoc")
+                            where int.Parse(c.Attribute("Chuong").Value) == iChuong && int.Parse(c.Attribute("Bai").Value) == iBai  
+                            select c;
+            if (baiHocThu.Count() != 0)
+            {
+                XElement BaiHoc = baiHocThu.ElementAt(0);
+                int SoCau = BaiHoc.Elements().Count();
+                for (int i = 0; i < SoCau; i++)
+                {
+                    XElement cau = BaiHoc.Elements().ElementAt(i);
+                    switch (cau.Name.ToString())
+                    {
+                        case "TracNghiem":
+                            TracNghiem temp = new TracNghiem();
+                            string path = AppDomain.CurrentDomain.BaseDirectory + cau.Attribute("Source").Value;
+                            string strThuTuCau = cau.Attribute("Cau").Value;
+                            temp.LayCauHoi(path,strThuTuCau, 15, 50);
+                            this.spDanhSachCauHoi.Children.Add(temp);
+                            break;
+                        case "SapXepChungMinh":
+                            //tao câu sắp xếp chứng minh
+                            break;
+
+                    }
+                }
+                Button btchamDiem = new Button();
+                btchamDiem.Width = 100;
+                btchamDiem.Height = 30;
+                btchamDiem.Content = "Chấm điểm";
+                btchamDiem.Click += new RoutedEventHandler(btchamDiem_Click);
+                this.spDanhSachCauHoi.Children.Add(btchamDiem);
+            }
+        }
+
+        void btchamDiem_Click(object sender, RoutedEventArgs e)
+        {
+            // chidlren cuối cùng là button chấm điểm
+            double Diem = 0;
+            int socau = 0;
+            for (int i = 0; i < this.spDanhSachCauHoi.Children.Count; i++)
+            {
+                string temp = spDanhSachCauHoi.Children[i].GetType().ToString();
+                if (temp == "ColorSwatch.TracNghiem")
+                {
+                    Diem += (spDanhSachCauHoi.Children[i] as TracNghiem).ChamDiem(1);
+                    socau++;
+                }
+                // nếu là bìa sắp xếp chứng minh
+            }
+            MessageBox.Show((Diem).ToString());//tùy cách tổ chức thang điểm. cách chấm
+        }
+    }
+}
---
+++ 6. Source Code/Source_BanFinal2/TracNghiem/UC/UCMucLuc.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TracNghiem.UC
+{
+    /// <summary>
+    /// Interaction logic for UCMucLuc.xaml
+    /// </summary>
+    public partial class UCMucLuc : UserControl
+    {
+        public UCMucLuc()
+        {
+            InitializeComponent();
+        }
+
+        private void DaiSo_Chuong1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void DaiSo_Chuong2_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void DaiSo_Chuong3_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void DaiSo_Chuong4_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void DaiSo_Chuong5_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void DaiSo_De1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void DaiSo_De2_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void DaiSo_De3_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void DaiSo_De4_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void DaiSo_De5_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void HinhHoc_Chuong1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void HinhHoc_Chuong2_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void HinhHoc_Chuong3_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void HinhHoc_Chuong4_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void HinhHoc_Chuong5_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void HinhHoc_De1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void HinhHoc_De2_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void HinhHoc_De3_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void HinhHoc_De4_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void HinhHoc_De5_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
