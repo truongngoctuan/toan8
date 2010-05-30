33199b486fc68d32b90836a29f98cc8abe35eb3c
kitminh25251325 <kitminh25251325@04bc2280-195f-c101-2899-a8bbd5de0aab>
2010-05-30 18:37:20 +0000

    Vòng Sĩ Kịt - sourceVòng Sĩ Kịt - [6.Source/Source_BanFinal] add project trăc nghiem khác

---
+++ trunk/6. Source Code/Source_BanFinal/TracNghiem/Comparer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TracNghiem
+{
+    public class Comparer
+    {
+        public static int sapxepChuong(string s1, string s2)
+        {
+            if (s1.Length > s2.Length)
+            {
+                return 1;
+            }
+
+            if (s1.Length == s2.Length)
+            {
+                return s1.CompareTo(s2);
+            }
+            return -1;
+        }
+        public static int sapxepDe(string s1, string s2)
+        {
+            string[] Sp1 = s1.Split('\\', '.', '/');
+            string[] Sp2 = s2.Split('\\', '.', '/');
+
+            s1 = Sp1[Sp1.Length - 2];
+            s2 = Sp2[Sp2.Length - 2];
+
+            string[] sp1 = s1.Split('_');
+            string[] sp2 = s2.Split('_');
+            if (int.Parse(sp1[0]) > int.Parse(sp2[0]))
+            {
+                return 1;
+            }
+            else if (sp1[0] == sp2[0])
+            {
+                if (sp1.Length > sp2.Length)
+                {
+                    return 1;
+                }
+
+                if (sp1.Length == sp2.Length)
+                {
+                    if (sp1.Length == 1)
+                    {
+                        return 0;
+                    }
+
+                    if (int.Parse(sp1[1]) > int.Parse(sp2[1]))
+                    {
+                        return 1;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ trunk/6. Source Code/Source_BanFinal/TracNghiem/De.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows;
+using System.Windows.Media;
+
+namespace TracNghiem
+{
+    public class De
+    {
+        public string m_Ten;
+        public List<ListBoxItem> m_DanhSachCau;
+        public List<bool> m_TinhTrangCheck;
+
+        public De(string folderPath, double width)
+        {
+            string fileDapAn = Directory.GetFiles(folderPath, "Dap*.txt")[0];
+            string[] fileDe = Directory.GetFiles(folderPath, "*.png");
+            List<string> dsFilede = new List<string>(fileDe);
+            dsFilede.Sort(Comparer.sapxepDe);
+            m_DanhSachCau = new List<ListBoxItem>();
+            StreamReader sr = new StreamReader(fileDapAn);
+            m_Ten = sr.ReadLine().Trim();
+            m_TinhTrangCheck = new List<bool>();
+            int nDapAn = 0;
+            int nPhuongAn = 0;
+            string groupName = "";
+            
+            for (int i = 0; i < dsFilede.Count; ++i)
+            {
+                string[] fileNameparts = dsFilede[i].Split('/', '\\', '.');
+                string[] namepart = fileNameparts[fileNameparts.Length - 2].Split('_');
+
+                if (namepart.Length == 1)
+                {
+                    groupName = i.ToString();
+                    m_TinhTrangCheck.Add(bool.Parse(sr.ReadLine().Trim()));
+                    ListBoxItem lbi = new ListBoxItem();
+                    StackPanel sp = new StackPanel();
+                    BitmapSource bs = new BitmapImage(new Uri(dsFilede[i]));
+                    Image img = new Image();
+                    img.Source = bs;
+                    img.Height = bs.Height;
+                    img.Width = bs.Width;
+                    sp.Height = bs.Height;
+                    sp.Width = bs.Width;
+                    sp.Tag = "de";
+                    lbi.Height = bs.Height;
+                    lbi.Width = width;
+                    sp.Width = width;
+                    sp.Orientation = Orientation.Horizontal;
+                    sp.Children.Add(img);
+                    lbi.Content = sp;
+                    m_DanhSachCau.Add(lbi);
+
+                    if (nDapAn > 1)
+                    {
+                        for (int j = i - nPhuongAn; j < i; ++j)
+                        {
+                            m_DanhSachCau.RemoveAt(j);
+                            ListBoxItem lbiPhuongAn = new ListBoxItem();
+                            StackPanel spPhuongAn = new StackPanel();
+                            spPhuongAn.Tag = "cb";
+                            CheckBox cb = new CheckBox();
+                            Canvas cv = new Canvas();
+                            cv.Children.Add(cb);
+                            cb.IsChecked = false;
+                            BitmapSource bsPhuongAn = new BitmapImage(new Uri(dsFilede[j]));
+                            Image imgPhuongAn = new Image();
+                            imgPhuongAn.Source = bsPhuongAn;
+                            imgPhuongAn.Height = bsPhuongAn.Height;
+                            imgPhuongAn.Width = bsPhuongAn.Width;
+                            spPhuongAn.Height = bsPhuongAn.Height;
+                            spPhuongAn.Width = bsPhuongAn.Width;
+                            cb.Width = 25;
+                            cb.Height = 25;
+                            cb.Arrange(new Rect(0, 0, cb.Width, cb.Height));
+                            cb.Margin = new Thickness(10, (bsPhuongAn.Height - cb.Height) / 2 + 6, 0, 0);
+                            cv.Width = cb.Width + 30;
+                            cv.Height = bsPhuongAn.Height;
+
+                            lbiPhuongAn.Height = bsPhuongAn.Height;
+                            lbiPhuongAn.Width = width - 60;
+                            spPhuongAn.Orientation = Orientation.Horizontal;
+                            spPhuongAn.Width = width;
+                            spPhuongAn.Children.Add(cv);
+                            spPhuongAn.Children.Add(imgPhuongAn);
+                            lbiPhuongAn.Content = spPhuongAn;
+                            m_DanhSachCau.Insert(j, lbiPhuongAn);    
+                        }
+                    }
+                    nDapAn = 0;
+                    nPhuongAn = 0;
+                }
+                else
+                {
+                    m_TinhTrangCheck.Add(bool.Parse(sr.ReadLine().Trim()));
+                    ++nPhuongAn;
+                    if (m_TinhTrangCheck[i])
+                    {
+                        ++nDapAn;
+                    }
+                    ListBoxItem lbi = new ListBoxItem();
+                    StackPanel sp = new StackPanel();
+                    sp.Tag = "rd";
+                    RadioButton rd = new RadioButton();
+                    rd.GroupName = groupName;
+                    Canvas cv = new Canvas();
+                    rd.IsChecked = false;
+                    BitmapSource bs = new BitmapImage(new Uri(dsFilede[i]));
+                    Image img = new Image();
+                    img.Source = bs;
+                    img.Height = bs.Height;
+                    img.Width = bs.Width;
+                    sp.Height = bs.Height;
+                    sp.Width = bs.Width;
+                    rd.Width = 25;
+                    rd.Height = 25;
+                    rd.Arrange(new Rect(0, 0, rd.Width, rd.Height));
+                    rd.Margin = new Thickness(10, (bs.Height - rd.Height)/2 + 6, 0, 0);
+                    cv.Width = rd.Width + 30;
+                    cv.Height = bs.Height;
+                    cv.Children.Add(rd);                    
+                    lbi.Height = bs.Height;
+                    lbi.Width = width - 60;
+                    sp.Orientation = Orientation.Horizontal;
+                    sp.Width = width;
+                    sp.Children.Add(cv);
+                    sp.Children.Add(img);
+                    lbi.Content = sp;
+                    m_DanhSachCau.Add(lbi);
+
+                    if (i == dsFilede.Count - 1 && nDapAn > 1)
+                    {
+                        for (int j = i - nPhuongAn + 1; j <= i; ++j)
+                        {
+                            m_DanhSachCau.RemoveAt(j);
+                            ListBoxItem lbiPhuongAn = new ListBoxItem();
+                            StackPanel spPhuongAn = new StackPanel();
+                            spPhuongAn.Tag = "cb";
+                            CheckBox cb = new CheckBox();
+                            Canvas cbcv = new Canvas();
+                            cbcv.Children.Add(cb);
+                            cb.IsChecked = false;
+                            BitmapSource bsPhuongAn = new BitmapImage(new Uri(dsFilede[j]));
+                            Image imgPhuongAn = new Image();
+                            imgPhuongAn.Source = bsPhuongAn;
+                            imgPhuongAn.Height = bsPhuongAn.Height;
+                            imgPhuongAn.Width = bsPhuongAn.Width;
+                            spPhuongAn.Height = bsPhuongAn.Height;
+                            spPhuongAn.Width = bsPhuongAn.Width;
+                            cb.Width = 25;
+                            cb.Height = 25;
+                            cb.Arrange(new Rect(0, 0, cb.Width, cb.Height));
+                            cb.Margin = new Thickness(10, (bsPhuongAn.Height - cb.Height) / 2 + 6, 0, 0);
+                            cbcv.Width = cb.Width + 30;
+                            cbcv.Height = bsPhuongAn.Height;
+
+                            lbiPhuongAn.Height = bsPhuongAn.Height;
+                            lbiPhuongAn.Width = width - 60;
+                            spPhuongAn.Orientation = Orientation.Horizontal;
+                            spPhuongAn.Width = width;
+                            spPhuongAn.Children.Add(cbcv);
+                            spPhuongAn.Children.Add(imgPhuongAn);
+                            lbiPhuongAn.Content = spPhuongAn;
+                            m_DanhSachCau.Insert(j, lbiPhuongAn);
+                        }
+                        nDapAn = 0;
+                        nPhuongAn = 0;
+                    }
+                }
+            }
+            sr.Close();
+        }        
+    }
+}
