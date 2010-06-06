564789067900350a39afa7ebb1b545ce46b0f76f
lnhthu <lnhthu@04bc2280-195f-c101-2899-a8bbd5de0aab>
2010-06-06 13:06:56 +0000

    UnitTest phần sắp xếp chứng minh

--- 6. Source Code/Source_BanFinal/MyUnitTest/UnitTestSapXepChungMinh.cs
+++ 6. Source Code/Source_BanFinal/MyUnitTest/UnitTestSapXepChungMinh.cs
@@ -3,19 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
-
+//using MainMenu;
+using ColorSwatch;
 namespace MyUnitTest
 {
-    //[TestFixture]
-    //public class UnitTestSapXepChungMinh
-    //{
-    //    [Test]
-    //    public void TestLaySoCauHoi()
-    //    {
-    //        int iSoCauHoi;
-    //        Window1 SXCMDS = new Window1();
-    //        iSoCauHoi = SXCMDS.LaySoCauHoiTrongMotBai();
-    //        Assert.AreEqual(2, iSoCauHoi);
-    //    }
-    //}
+    [TestFixture]
+    public class UnitTestSapXepChungMinh
+    {
+        SapXepChungMinh sapXepChungMinh = new SapXepChungMinh();
+        [SetUp]
+        public void KhoiTao()
+        {
+            sapXepChungMinh.m_Path = "DeSapXepChungMinh\\BaiTapDaiSo\\Chuong01";
+            sapXepChungMinh.m_BaiHoc = 1;
+            sapXepChungMinh.m_CauHoi = 1;
+            sapXepChungMinh.m_Chuong = 1;
+        }
+        // test lay noi dung cau hoi dai so cau 1 bai 1 chuong 1.
+        [Test]
+        public void TestLaySoCauHoi()
+        {
+            sapXepChungMinh.LayDuLieuCauHoi();
+            string ketQuaMongDoi = "DS_C1_B1_C1.png";
+            Assert.AreEqual(ketQuaMongDoi, sapXepChungMinh.m_StrCauHoi);
+        }
+        // test lay dap an cua cau 1, bai 1, chuong 1 dai so
+        public void TestLayDuLieuDapAn()
+        {
+            List<string> ketQuaMongDoi = new List<string>();
+            ketQuaMongDoi.Add("DS_C1_B1_C1_DA1.png");
+            ketQuaMongDoi.Add("DS_C1_B1_C1_DA2.png");
+            sapXepChungMinh.LayDuLieuDapAn();
+            Assert.AreEqual(ketQuaMongDoi, sapXepChungMinh.m_ListStrDapAn);
+        }
+    }
 }
